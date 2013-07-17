5cf0ee4e41088f3afc42fabcbe6dbd94ab947925
Andrew McGregor <me@andrewmcgregor.me>
2013-07-17 16:00:38 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Otterflow::Application.config.secret_token = '40b00ad79e25ad10d4257c068c67ca9fc65f98a67892299b4a7a0eeefea9d9c895d6068e74d1b7e553bb64fac7cb8fd9f19ce41a7e52c1967dabc30633f734b1'
