92915fb3593361e75a35ccec4fbb67d993e60c36
Tyler Shipe <tjshipe@gmail.com>
2013-07-18 15:45:04 -0500

    fixed comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,26 @@
 class CommentsController < ApplicationController
 
-  before_filter :authenticate # which pages should we authenticate?
+  before_filter :authorize
 
   def new
     @comment = Comment.new
   end
 
   def create
-    if params[:comment_id]
-      commentable = Comment.find(params[:comment_id])
+    if params[:question_id]
+      commentable = Question.find(params[:comment_id]) # put into helper method
+      url = commentable
     elsif params[:answer_id]
       commentable = Answer.find(params[:answer_id])
+      url = answer.question
     end
 
     @comment = commentable.comments.new(params[:comment])
 
     if @comment.save
-      redirect_to question_url()
+      redirect_to question_url(url)
     else
-      render :new # tell why it didn't save
+      render :new, notice: "Your comment was invalid."
     end
   end
 
@@ -27,19 +29,35 @@ class CommentsController < ApplicationController
   end
 
   def update
+    if params[:question_id]
+      commentable = Question.find(params[:comment_id])
+      url = commentable
+    elsif params[:answer_id]
+      commentable = Answer.find(params[:answer_id])
+      url = answer.question
+    end
+
     @comment = Comment.find(params[:id])
 
     if @comment.update_attributes(params[:comment])
-      redirect_to question_url(@question)
+      redirect_to question_url(url)
     else
       render :edit
     end
   end
 
   def destroy
+    if params[:question_id]
+      commentable = Question.find(params[:comment_id])
+      url = commentable
+    elsif params[:answer_id]
+      commentable = Answer.find(params[:answer_id])
+      url = answer.question
+    end
+
     comment = Comment.find(params[:id])
     comment.destroy
 
-    redirect_to question_url
+    redirect_to question_url(url)
   end
 end
