35f374f08d19afa14ed9ff88f3a5c06b40fe6c70
Andrew McGregor <me@andrewmcgregor.me>
2013-07-18 15:37:09 -0500

    Shifted authenticate + authorize methods from Users helper to Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   include ApplicationHelper
+
+  def authorize
+    redirect_to root_url, notice: "You do not have permission to do this!" if current_user.id != params[:id]
+  end
+
+  def authenticate
+    current_user
+  end
 end
