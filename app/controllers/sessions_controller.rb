b0b2e08a298d5829a810a4e00d906c05314d0ac1
Andrew McGregor <me@andrewmcgregor.me>
2013-07-18 10:28:31 -0500

    Finished basic sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+	def new
+	end
+
+	def create
+		@user = User.find_by_username(params[:username])
+    if @user.password == params[:password]
+      session[:user_id] = @user.id
+      redirect_to user_path(@user), notice: "Successfully logged in :)"
+    else
+      redirect_to root_url, notice: "Invalid username and/or password :("
+    end
+	end
+
+	def destroy
+		sessions.clear
+		redirect_to root_url, notice: "Successfully logged out :)"
+	end
+end
