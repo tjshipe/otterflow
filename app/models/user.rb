8dd84695ad505c55617c1a624a031e6a75f55df5
Andrew McGregor <me@andrewmcgregor.me>
2013-07-18 09:55:02 -0500

    Add password and password= methods to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,15 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true, uniqueness: true
   validates_presence_of :password_hash
+
+  require 'bcrypt'
+
+  def password
+    @password ||= BCrypt::Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.password_hash = @password
+  end
 end
