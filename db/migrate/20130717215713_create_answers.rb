1b80df328d53e7e12be5969098492aed1ba87c0d
tjshipe <tjshipe@gmail.com>
2013-07-17 19:16:22 -0500

    added to answers migration the question field and removed voteable

--- db/migrate/20130717215713_create_answers.rb
+++ db/migrate/20130717215713_create_answers.rb
@@ -3,9 +3,8 @@ class CreateAnswers < ActiveRecord::Migration
   	create_table :answers do |t|
   		t.text :text, null: false
   		t.boolean :chosen?, default: false
-
+      t.belongs_to :question
   		t.belongs_to :user
-  		t.belongs_to :votable
 
   		t.timestamps
   	end
