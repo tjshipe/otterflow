9aebc98e7dc6dc68ba3e5d309a77ad789ca35e86
Tyler Shipe <tjshipe@gmail.com>
2013-07-17 20:03:07 -0500

    removed invalid characters from votes and answers migration

--- db/migrate/20130717215713_create_answers.rb
+++ db/migrate/20130717215713_create_answers.rb
@@ -2,7 +2,7 @@ class CreateAnswers < ActiveRecord::Migration
   def change
   	create_table :answers do |t|
   		t.text :text, null: false
-  		t.boolean :chosen?, default: false
+  		t.boolean :chosen, default: false
       t.belongs_to :question
   		t.belongs_to :user
 
