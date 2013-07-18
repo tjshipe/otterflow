59433e6a7e06dc18abcd9c3daab81937d080a427
Andrew McGregor <me@andrewmcgregor.me>
2013-07-18 10:07:34 -0500

    Add basic users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.find_by_username(params[:username])
+    if @user.password == params[:password]
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to root_url
+    end
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		@user.update_attributes(params[:user])
+		redirect_to user_path(@user)
+	end
+
+	def destroy
+		User.find(session[:user_id]).destroy
+		sessions.clear
+		redirect_to root_url
+	end
+end
