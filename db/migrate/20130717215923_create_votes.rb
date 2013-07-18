210b2a97ec95ad0b45c15205ca07871308c7cf1b
tjshipe <tjshipe@gmail.com>
2013-07-17 19:10:33 -0500

    added polymorphic association 'voteable'

--- db/migrate/20130717215923_create_votes.rb
+++ db/migrate/20130717215923_create_votes.rb
@@ -2,7 +2,7 @@ class CreateVotes < ActiveRecord::Migration
   def change
   	create_table :votes do |t|
   		t.boolean :positive?
-
+      t.references :voteable, polymorphic: true
   		t.belongs_to :question
   		t.belongs_to :user
 
