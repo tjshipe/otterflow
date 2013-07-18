63d81c5f85c7c987bc3b14937f77ebf130ae439f
tjshipe <tjshipe@gmail.com>
2013-07-17 19:13:28 -0500

    removed belong_to question field in votes migration

--- db/migrate/20130717215923_create_votes.rb
+++ db/migrate/20130717215923_create_votes.rb
@@ -3,7 +3,6 @@ class CreateVotes < ActiveRecord::Migration
   	create_table :votes do |t|
   		t.boolean :positive?
       t.references :voteable, polymorphic: true
-  		t.belongs_to :question
   		t.belongs_to :user
 
   		t.timestamps
