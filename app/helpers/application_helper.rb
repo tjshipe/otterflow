2615a785188fde68f4b029cc8bfd5abf493b8ea8
Andrew McGregor <me@andrewmcgregor.me>
2013-07-18 15:39:55 -0500

    fix indentation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,12 @@ module ApplicationHelper
 	def signed_in?
 		current_user
 	end
+
+  def authorize
+    redirect_to root_url, notice: "You do not have permission to do this!" if current_user.id != params[:id]
+  end
+
+  def authenticate
+    current_user
+  end
 end
