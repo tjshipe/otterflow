73dacdbf2a86f4aa84b9e343dfe7a602da843038
Andrew McGregor <me@andrewmcgregor.me>
2013-07-18 15:38:01 -0500

    Move previous commit methods to application helper instead

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   include ApplicationHelper
-
-  def authorize
-    redirect_to root_url, notice: "You do not have permission to do this!" if current_user.id != params[:id]
-  end
-
-  def authenticate
-    current_user
-  end
 end
