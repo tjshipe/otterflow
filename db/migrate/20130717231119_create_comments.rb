ca970398be5a723cb272fa1f9532ce7b8cb5b209
tjshipe <tjshipe@gmail.com>
2013-07-17 19:08:20 -0500

    fixed polymorphic association syntax

--- db/migrate/20130717231119_create_comments.rb
+++ db/migrate/20130717231119_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration
   	create_table :comments do |t|
   		t.text :text
 
-  		t.belongs_to :commentable
+  		t.references :commentable, polymorphic: true
   		t.belongs_to :user
   		
   		t.timestamps
