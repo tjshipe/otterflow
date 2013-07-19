386c7d3e4bfa12db2945050775abb09b983463c5
claubacher <claubacher@gmail.com>
2013-07-19 16:07:07 -0500

    Removed debugging lines and changed user_id to current_user.id

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,27 +1,12 @@
 class VotesController < ApplicationController
   def create
-    p params
-
-    if params[:voteType] == 'question'
-      @object = Question.find(params[:object_id])
-    else
-      @object = Answer.find(params[:object_id])
-    end
-    p @object.sum
-
     if params[:voteDirection] == 'upvote'
       voteDirection = 1
     else
       voteDirection = -1
     end
     # Need to set user_id to current user
-    @vote = Vote.new(value: voteDirection, user_id: 1)
-
-    if params[:voteType] == 'question'
-      @vote.voteable = Question.find(params[:object_id])
-    else
-      @vote.voteable = Answer.find(params[:object_id])
-    end
+    @vote = Vote.new(value: voteDirection, user_id: current_user.id)
 
     if @vote.save
       if params[:voteType] == 'question'
@@ -29,7 +14,6 @@ class VotesController < ApplicationController
       else
         @vote.voteable = Answer.find(params[:object_id])
       end
-       p @vote.voteable.sum
       respond_to do |format|
         format.html do
           redirect_to @vote.voteable.question
@@ -41,15 +25,6 @@ class VotesController < ApplicationController
     else
       render :text => "didn't work"
     end
-    p @vote.voteable.sum
-    # positive = params[:positive]
-    # vote = current_user.votes.new(:positive => positive)
-
-    # if params[:question_id]
-    #   vote.voteable = Question.find(params[:question_id])
-    # elsif params[:answer_id]
-    #   vote.voteable = Answer.find(params[:answer_id])
-    # end
   end
 
   def destroy
