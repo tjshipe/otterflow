44b6015b5227b3b51896b293972f7d76a42ea030
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-19 18:40:11 -0500

    add one vote per question

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,24 +1,50 @@
 class VotesController < ApplicationController
   def create
+    input = params.clone
+    p input
+    if input[:voteType] == 'question'
+      input[:voteType] = 'Question'
+    elsif input[:voteType] == 'answers'
+      input[:voteType] = 'Answer' 
+    end  
+    if params[:voteDirection] == 'upvote'
+      input[:voteDirection] = 1
+    else
+      input[:voteDirection] = -1
+    end
+
     if params[:voteDirection] == 'upvote'
       voteDirection = 1
     else
       voteDirection = -1
     end
-    # Need to set user_id to current user
-    @vote = Vote.new(value: voteDirection, user_id: current_user.id)
-
-    if params[:voteType] == 'question'
-      @vote.voteable = Question.find(params[:object_id])
+    @existing_vote = Vote.where("user_id = ? AND voteable_type = ? AND voteable_id = ?", current_user.id, input[:voteType], input[:object_id])[0]
+    if @existing_vote != nil && @existing_vote.value != voteDirection
+      @vote = @existing_vote
+      @vote.value = voteDirection
+    elsif @existing_vote != nil && @existing_vote.value == voteDirection
+      @vote = @existing_vote
+      @vote.value = 0
     else
-      @vote.voteable = Answer.find(params[:object_id])
+      @vote = Vote.new(value: voteDirection, user_id: current_user.id)
+      @vote.value = voteDirection
     end
 
-    if @vote.save
       if params[:voteType] == 'question'
         @vote.voteable = Question.find(params[:object_id])
       else
         @vote.voteable = Answer.find(params[:object_id])
+      end
+
+  
+  
+    if @vote.save
+      if params[:voteType] == 'question'
+        @vote.voteable = Question.find(params[:object_id])
+        @vote.value = voteDirection
+      else
+        @vote.voteable = Answer.find(params[:object_id])
+        @vote.value = voteDirection
       end
       respond_to do |format|
         format.html do
