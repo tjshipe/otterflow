4c48a81cfa6577342a7f2f13a31b93e22c36f5c1
Tyler Shipe <tjshipe@gmail.com>
2013-07-19 14:11:11 -0500

    created user_spec file for integration test

---
+++ spec/requests/user_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe 'User' do
+
+  before do
+    @user = User.new(username: "user", description: "I love cats.")
+    @user.password = "password"
+    @user.save
+    @question = Question.create(title: "What is this site for?", body: "I don't understand..", user_id: @user.id)
+    visit questions_url
+  end
+
+  context "on homepage" do
+    it "sees a list of questions" do
+      page.should have_content "Top Questions"
+    end
+    it "can click on a login link" do
+      click_link "Login"
+      current_path.should eq new_session_path
+    end
+    it "can click on a question title to go to question page" do
+      click_link "#{@question.title}"
+      current_path.should eq question_path(@question)
+    end
+    it "sees the username for who created the question" do
+      page.should have_content "#{@question.user.username}"
+    end
+
+  end
+
+  context "on question page" do
+    it "sees a question title" do
+      click_link "#{@question.title}"
+    end
+  end
+
+
+end
